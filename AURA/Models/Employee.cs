f0c1748c16d39e90edbed189e181d51241b6e316
HOME <HOME@DESKTOP-0JAKMNT>
2024-06-26 11:14:33 +0530

    Add project files.

---
+++ AURA/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿namespace HRM.Models
+{
+    public class Employee
+    {
+        public Guid Candidate_id { get; set; }// guid is a return type like int its create the randome different numbers upto 16 digits.
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+
+        public string genger { get; set; } = "";
+        public string Address { get; set; } = "";
+        public int Age { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public decimal Phone_no { get; set; }
+    }
+}
